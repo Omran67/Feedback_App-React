97e2161d36a019718f836134b03a56caf8f7dc76
Omran67 <alhaririomran12@gmail.com>
2022-11-03 15:22:29 +0100

    'fourth⚡️ Improve performance.🎨 Improve structure / format of the code.

--- udemy-project/src/components/FeedbackList.jsx
+++ udemy-project/src/components/FeedbackList.jsx
@@ -2,20 +2,19 @@ import FeedbackItem from './FeedbackItem';
 import {useContext} from 'react';
 import {motion, AnimatePresence} from 'framer-motion';
 import FeedbackContext from '../context/FeedbackContext';
-import Spinner from './shared/Spinner'
+
 
 
 function FeedbackList() {
-    const {feedback,  isLoading} = useContext(FeedbackContext)
+    const {feedback} = useContext(FeedbackContext)
 
-    if (!isLoading && (!feedback || feedback.length === 0)) {
+    if (!feedback || feedback.length === 0) {
         return <p> No feedback yet</p>
     }
-    return isLoading ? <h3>Loading....</h3> : (<div className='feedback-list'>
+    return (<div className='feedback-list'>
         <AnimatePresence>
             {feedback.map((item)=>(
                 <motion.div key = {item.id}
-                            key={item.id}
                             initial = {{opacity: 0}}
                             animate = {{opacity: 1}}
                             exit={{opacity:0}}
